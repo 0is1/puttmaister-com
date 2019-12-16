822bf3a2a7406b036eeabfe21854b04be7b32fba
Janne Saarela <janne.juhani@gmail.com>
2019-12-16 22:38:58 +0200

    Add initial privacy policy

---
+++ src/pages/privacy.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { P } from "components/Typography";
+
+export default () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1 style={{ fontSize: "2rem", padding: "2rem 0" }}>
+        Privacy Policy for Putt Maister
+      </h1>
+
+      <P margin="0">
+        At Putt Maister, accessible from https://puttmaister.com, one of our
+        main priorities is the privacy of our visitors. This Privacy Policy
+        document contains types of information that is collected and recorded by
+        Putt Maister and how we use it.
+      </P>
+
+      <P margin="0">
+        If you have additional questions or require more information about our
+        Privacy Policy, do not hesitate to contact us.
+      </P>
+
+      <h2 style={{ fontSize: "1.5rem", padding: "2rem 0 1rem" }}>
+        Privacy Policies
+      </h2>
+
+      <P margin="0">
+        You may consult this list to find the Privacy Policy for each of the
+        advertising partners of Putt Maister. Our Privacy Policy was created
+        with the help of the{" "}
+        <a href="https://www.privacypolicygenerator.info">
+          Privacy Policy Generator
+        </a>{" "}
+        and the{" "}
+        <a href="https://www.privacypolicyonline.com">
+          Privacy Policy Generator Online
+        </a>
+        .
+      </P>
+
+      <P margin="0">
+        Third-party ad servers or ad networks uses technologies like cookies,
+        JavaScript, or Web Beacons that are used in their respective
+        advertisements and links that appear on Putt Maister, which are sent
+        directly to users' browser. They automatically receive your IP address
+        when this occurs. These technologies are used to measure the
+        effectiveness of their advertising campaigns and/or to personalize the
+        advertising content that you see on websites that you visit.
+      </P>
+
+      <P margin="0">
+        Note that Putt Maister has no access to or control over these cookies
+        that are used by third-party advertisers.
+      </P>
+
+      <h2 style={{ fontSize: "1.5rem", padding: "2rem 0 1rem" }}>
+        Third Party Privacy Policies
+      </h2>
+
+      <P margin="0">
+        Putt Maister's Privacy Policy does not apply to other advertisers or
+        websites. Thus, we are advising you to consult the respective Privacy
+        Policies of these third-party ad servers for more detailed information.
+        It may include their practices and instructions about how to opt-out of
+        certain options. You may find a complete list of these Privacy Policies
+        and their links here: Privacy Policy Links.
+      </P>
+
+      <P margin="0">
+        You can choose to disable cookies through your individual browser
+        options. To know more detailed information about cookie management with
+        specific web browsers, it can be found at the browsers' respective
+        websites. What Are Cookies?
+      </P>
+
+      <h2 style={{ fontSize: "1.5rem", padding: "2rem 0 1rem" }}>
+        Children's Information
+      </h2>
+
+      <P margin="0">
+        Another part of our priority is adding protection for children while
+        using the internet. We encourage parents and guardians to observe,
+        participate in, and/or monitor and guide their online activity.
+      </P>
+
+      <P margin="0">
+        Putt Maister does not knowingly collect any Personal Identifiable
+        Information from children under the age of 13. If you think that your
+        child provided this kind of information on our website, we strongly
+        encourage you to contact us immediately and we will do our best efforts
+        to promptly remove such information from our records.
+      </P>
+
+      <h2 style={{ fontSize: "1.5rem", padding: "2rem 0 1rem" }}>
+        Online Privacy Policy Only
+      </h2>
+
+      <P margin="0">
+        This Privacy Policy applies only to our online activities and is valid
+        for visitors to our website with regards to the information that they
+        shared and/or collect in Putt Maister. This policy is not applicable to
+        any information collected offline or via channels other than this
+        website.
+      </P>
+
+      <h2 style={{ fontSize: "1.5rem", padding: "2rem 0 1rem" }}>Consent</h2>
+
+      <P margin="0">
+        By using our website, you hereby consent to our Privacy Policy and agree
+        to its Terms and Conditions.
+      </P>
+    </div>
+  );
+};
