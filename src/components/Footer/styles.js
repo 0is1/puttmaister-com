c86f897b6f5ed99f35e5c880027bac16f2a530a0
Janne Saarela <janne.saarela@kehittamo.fi>
2019-12-21 20:05:31 +0200

    Make footer fixed

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,20 +1,22 @@
 import styled from "@emotion/styled";
-import { LG_BREAKPOINT } from "helpers/variables";
 import { white } from "constants/colors";
 
 export const FooterContainer = styled.footer`
   background: ${white};
+  bottom: 0;
   box-shadow: 0px -1px 6px 0px rgba(0, 0, 0, 0.4);
+  left: 0;
+  position: fixed;
+  right: 0;
+  z-index: 9;
 `;
 export const FooterWrapper = styled.div`
   align-items: center;
-  padding: 1.45rem 1.5rem;
+  padding: 0.25rem 1.5rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  @media (min-width: ${LG_BREAKPOINT}) {
-    flex-direction: row;
-  }
+  flex-direction: row;
 `;
 
 export const A = styled.a`
